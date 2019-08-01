918eea495a0f60fe612ac48bcbb4c2f2ce00e78a
grobot <1157215781@qq.com>
2019-08-01 15:35:58 +0800

    over part 4

--- darknet.py
+++ darknet.py
@@ -149,7 +149,7 @@ class Darknet(nn.Module):
         super(Darknet,self).__init__()
         self.blocks = parse_cfg(cfgfile)
         self.net_info,self.module_list = create_modules(self.blocks)
-
+        # print(self.module_list)
     # 读取cfg里的网络信息，根据不同的网络配置进行对应的forward操作。
     # nn.Sequential 是可以直接进行forward操作的，nn.module_list 只是保存模型，所以可以直接调用module_list[i](x)进行forward操作
 
@@ -206,6 +206,73 @@ class Darknet(nn.Module):
 
         return detections
 
+    def load_weights(self,weightfile):
+
+        fp = open(weightfile,"rb")
+
+        header = np.fromfile(fp,dtype = np.int32,count = 5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+
+        weights = np.fromfile(fp,dtype = np.float32)
+        ptr = 0
+
+        for i in range(len(self.module_list)):
+            module_type = self.blocks[i+1]["type"]
+
+            if module_type == "convolutional":
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i+1]["batch_normalize"])
+                except:
+                    batch_normalize = 0
+                
+                conv = model[0]
+                if (batch_normalize):
+                    bn = model[1]
+                    num_bn_biases = bn.bias.numel()
+                    bn_biases = torch.from_numpy(weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_weights = torch.from_numpy(weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_mean = torch.from_numpy(weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_var = torch.from_numpy(weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_biases = bn_biases.view_as(bn.bias.data)
+                    bn_weights = bn_weights.view_as(bn.weight.data)
+                    bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                    bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                    bn.bias.data.copy_(bn_biases)
+                    bn.weight.data.copy_(bn_weights)
+                    bn.running_mean.copy_(bn_running_mean)
+                    bn.running_var.copy_(bn_running_var)
+
+                else:
+                    num_biases = conv.bias.numel()
+
+                    conv_biases = torch.from_numpy(weights[ptr:ptr+num_biases])
+                    ptr += num_biases
+
+                    conv_biases = conv_biases.view_as(conv.bias.data)
+                    conv.bias.data.copy_(conv_biases)
+                
+                num_weights = conv.weight.numel()
+                conv_weights = torch.from_numpy(weights[ptr:ptr+num_weights])
+                ptr += num_weights
+
+                conv_weights = conv_weights.view_as(conv.weight.data)
+                conv.weight.data.copy_(conv_weights)
+
+
+
+
+
 
 
 
@@ -213,10 +280,19 @@ class Darknet(nn.Module):
 
     
 if __name__ == "__main__":
+
+    # x = torch.Tensor([[1, 2, 3, 4],[2,2,3,3]])
+    # print(x,x.size())
+    # x = torch.unsqueeze(x, 0)
+    # print(x,x.size())
     model = Darknet("cfg/yolov3.cfg")
+    model.load_weights("./yolov3.weights")
+
     inp = get_test_input()
     pred = model(inp,torch.cuda.is_available())
-    print(pred,pred.size())
+    # print(pred)
+    rest = write_results(pred,0.5,80)
+    print(rest)
 
 
 
--- util.py
+++ util.py
@@ -8,6 +8,17 @@ from torch.autograd import Variable
 import numpy as np
 import cv2 
 
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+
+    return tensor_res
+
+
 def predict_transform(prediction, inp_dim, anchors, num_classes,CUDA = True):
 
     """
@@ -88,7 +99,140 @@ def predict_transform(prediction, inp_dim, anchors, num_classes,CUDA = True):
     prediction[:,:,:4] *= stride
 
     return prediction
+
+def bbox_iou(box1,box2):
+
+    b1_x1,b1_y1,b1_x2,b1_y2 = box1[:,0],box1[:,1],box1[:,2],box1[:,3]
+    b2_x1,b2_y1,b2_x2,b2_y2 = box2[:,0],box2[:,1],box2[:,2],box2[:,3]
+
+    inter_rect_x1 = torch.max(b1_x1,b2_x1)
+    inter_rect_y1 = torch.max(b1_y1,b2_y1)
+    inter_rect_x2 = torch.max(b1_x2,b2_x2)
+    inter_rect_y2 = torch.max(b1_y2,b2_y2)
+
+    inter_area = torch.clamp(inter_rect_x2-inter_rect_x1+1,min = 0)*torch.clamp(inter_rect_y2-inter_rect_y1+1,min=0)
+
+    b1_area = (b1_x2 - b1_x1+1)*(b1_y2-b1_y1+1)
+    b2_area = (b2_x2 - b2_x1+1)*(b2_y2-b2_y1+1)
+
+    iou = inter_area/(b1_area+b2_area-inter_area)
+
+    return iou
     
+def write_results(prediction,confidence,num_classes,nms_conf = 0.4):
+
+    """
+    NMS （非极大值抑制过程）
+    1.先把置信度比较低的过滤掉
+    2.改变维度，使输出只包含类别中最高得分及类别
+    3.取出图片中所有的类别，并按类别进行遍历
+    4.对置信度排序，并计算iou
+    5.去除重合率较高的预测，重复计算，直到对该类别的所有预测遍历完成
+    """
+
+    # 将置信度小于阀值的边界框设为零
+    conf_mask = (prediction[:,:,4] > confidence).float()
+    # print("conf mask1",conf_mask,conf_mask.size())
+    # unsqueeze就是 拓展维度
+    conf_mask = conf_mask.unsqueeze(2)
+    # print("conf mask2",conf_mask,conf_mask.size())
+    prediction = prediction*conf_mask
+
+    # 由（center，w,h)变为 （left,top,right,bottom) 
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
+    box_corner[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
+    box_corner[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2)
+    box_corner[:,:,3] = (prediction[:,:,0] + prediction[:,:,3]/2)
+    prediction[:,:,:4] = box_corner[:,:,:4]
+
+    batch_size = prediction.size(0)
+
+    write = False
+
+    # 遍历batch中的每个图片的检测结果
+    for ind in range(batch_size):
+        # 每个图片都有10627个结果
+        image_pred = prediction[ind]
+
+        # 取最大预测值，并把85保存为7（增加了分数和index，去掉了其他项）
+        # 最大值 和最大值索引
+        max_conf,max_conf_score = torch.max(image_pred[:,5:5+num_classes],1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+
+        seq = (image_pred[:,:5],max_conf,max_conf_score)
+        image_pred = torch.cat(seq,1)
+        # print("imagepred size1 ",image_pred.size())
+        # 去掉置信度被设为零的行,其实是取出不为零的行
+        non_zero_ind = (torch.nonzero(image_pred[:,4]))
+        # print(non_zero_ind.size())
+        try:
+            image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
+        except:
+            continue
+
+        # print("imagepred_ size2 ",image_pred_.size())
+        if image_pred_.shape[0] == 0:
+            continue
+
+        img_classes = unique(image_pred_[:,-1]) # 取出 类型ID 
+        # print("img class",img_classes)
+        for cls in img_classes:
+
+            #取出属于这个类别的所有预测
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+            image_pred_class = image_pred_[class_mask_ind].view(-1,7)
+            
+            # 按置信度进行排序，而不是得分
+            conf_sort_index = torch.sort(image_pred_class[:,4],descending = True)[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            # print("pred class",cls,image_pred_class)
+            idx = image_pred_class.size(0)
+
+            for i in range(idx):
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0),image_pred_class[i+1:])
+                except ValueError:
+                    break
+                except IndexError:
+                    break
+
+                # 去掉重合率较高的预测，所以对于有多个物体的类别，只要没有重合，就不会被过滤。
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                # print("iou ",cls,i,iou_mask)
+                image_pred_class[i+1:] *= iou_mask
+                non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                # print("iou non z",non_zero_ind)
+                image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+
+            # 把预测结果和图片在batch中的id对应起来
+            # new(image_pred_class.size(0),1)，这个是为了处理同一个类别有多个物体的情况，要把所有预测都和图片对应
+            batch_ind = image_pred_class.new(image_pred_class.size(0),1).fill_(ind)
+            # print("batch ind",batch_ind)
+            seq = batch_ind,image_pred_class
+
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+            # print(output)
+
+    try:
+        return output
+    except:
+        return 0
+
+
+
+
+
+    
+
 
 
 
